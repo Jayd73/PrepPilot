5d960776a3bfb5fbff068370802929a2da3eaa97
Jayd73 <jayeshdhuri777@gmail.com>
2025-02-01 21:23:00 +0530

    Added temprory workaround for sqlite on vercel

--- app/config.py
+++ app/config.py
@@ -4,7 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') 
+    # Use /tmp/ for SQLite in Vercel to allow writes
+    if os.environ.get("VERCEL"):  
+        SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/app.db"
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     QUESTION_IMG_UPLOAD_FOLDER = os.path.abspath('app/static/uploads/question_images')
     USER_AVATAR_UPLOAD_FOLDER = os.path.abspath('app/static/uploads/user_avatars')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
